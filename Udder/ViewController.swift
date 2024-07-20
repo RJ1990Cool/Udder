18c1871daac5d80aae7b8a8a32ec2ce1a45a4c32
Song Renjie <song.renjie1990412@gmail.com>
2024-07-20 19:59:33 +0900

    Add viewdid appear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -20,5 +20,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         //
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
 }
 
